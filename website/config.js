5ebffaeee2aa012fbd362276bec99e40f65799db
mapleshadow <mapleshadow@gmail.com>
2014-09-06 18:47:02 +0800

    Update

--- website/config.js
+++ website/config.js
@@ -1,18 +1,18 @@
 var api = "http://nomp.freeyy.me:8117";
 
-//var coinUnits = 1000000000000;
+//var coinUnits = 1000000000000;已被代码级别使用
 var poolHost = "nomp.freeyy.me";
 //币全网难度计算取值
-//var coinNetworkDiff = 60;
+//var coinNetworkDiff = 60;已被代码级别使用
 //币收益计算取值
 //var coinResl = 16;
-//12位数为以上默认值60和16、如果是8位数，一般取值为 240和160000
+//12位数为以上默认值60和16、如果是8位数，一般取值为 240和160000已被代码级别使用
 
 var irc = "irc.freenode.net/#poolhost";
 
 var email = "mapleshadow@gmail.com";
 
-var cryptonatorWidget = ["XDN-BTC", "XDN-LTC", "XDN-USD", "XDN-CNY"];
+var cryptonatorWidget = ["{symbol}-BTC", "{symbol}-LTC", "{symbol}-USD", "{symbol}-CNY"];
 
 var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
 
